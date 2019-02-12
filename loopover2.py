4f02913562bfb28b6e2b3e3131722c0eb7bdddde
Bryce Wilson <bryce@thenetworknerds.ca>
2019-02-12 15:01:40 -0800

    First working version
    Signed-off-by: Bryce Wilson <bryce@thenetworknerds.ca>

--- loopover2.py
+++ loopover2.py
@@ -7,85 +7,93 @@ import multiprocessing
 import itertools
 import defs2
 import tracemalloc
+
+
 # import os
 
 
 def split_boards(new_boards):
-	return set().union(*[newBoardsS[0] for newBoardsS in new_boards]), sum([countS[1] for countS in new_boards])
+    return set().union(*[newBoardsS[0] for newBoardsS in new_boards]), sum([countS[1] for countS in new_boards])
+
+
 # Real code starts below
 
 
 if __name__ == "__main__":
 
-	# boards = {((1,2,3,4,5),(6,7,8,9,10),(11,12,13,14,15),(16,17,18,19,20),(21,22,23,24,25))}
-	boards = {((1,2,3,4),(5,6,7,8),(9,10,11,12),(13,14,15,16))}
-	# boards = {((1,2,3),(4,5,6),(7,8,9))}
-	# boards = {((1,2),(3,4))}
+    # boards = {((1,2,3,4,5),(6,7,8,9,10),(11,12,13,14,15),(16,17,18,19,20),(21,22,23,24,25))}
+    boards = {((1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 16))}
+    # boards = {((1,2,3),(4,5,6),(7,8,9))}
+    # boards = {((1,2),(3,4))}
 
-	# boards = {((1,2,3,4,5),(6,7,8,9,10))}
-	# boards = {((1,2,3,4,5,6),(7,8,9,10,11,12))}
+    # boards = {((1,2,3,4,5),(6,7,8,9,10))}
+    # boards = {((1,2,3,4,5,6),(7,8,9,10,11,12))}
 
-	checkMem = False
-	pool = Pool(multiprocessing.cpu_count())
-	# pool = Pool(1)
+    checkMem = False
+    pool = Pool(multiprocessing.cpu_count())
+    # pool = Pool(1)
 
-	from datetime import datetime
-	timeStart = datetime.today()
-	found = True
-	number = 0
-	depth = 0
-	newBoards = copy.deepcopy(boards)
-	# oldBoards = set()
+    from datetime import datetime
 
-	if checkMem:
-		tracemalloc.start()
-	timeLast = datetime.today()
-	print("Init took: "+str(datetime.today()-timeStart))
-	while found:
-		# oldBoards = copy.deepcopy(newBoards)
-		newBoards, newCount = split_boards(pool.map(defs2.get_new_positions, newBoards, int(len(newBoards) / multiprocessing.cpu_count()) + 1))
+    timeStart = datetime.today()
+    found = True
+    number = 0
+    depth = 0
+    newBoards = copy.deepcopy(boards)
+    # oldBoards = set()
 
-		sames = set()
-		for board in defs2.InChunks(newBoards,multiprocessing.cpu_count()):
-			same = []
-			# for aBoard in defs2.InChunks(boards,multiprocessing.cpu_count()):
-			# same.append(board & aBoard)
-			sames.update(*pool.starmap(defs2.and_them, zip(itertools.repeat(board), defs2.InChunks(boards, multiprocessing.cpu_count())), int(len(newBoards) / multiprocessing.cpu_count()) + 1))
-			# same = set().union(*same)
-			# sames.update(same)
-		newBoards -= sames
+    if checkMem:
+        tracemalloc.start()
+    timeLast = datetime.today()
+    print("Init took: " + str(datetime.today() - timeStart))
+    while found:
+        # oldBoards = copy.deepcopy(newBoards)
+        newBoards, newCount = split_boards(
+            pool.map(defs2.get_new_positions, newBoards, int(len(newBoards) / multiprocessing.cpu_count()) + 1))
 
-		newBoards = newBoards - boards
-		boards.update(newBoards)
-		number += newCount
-		depth += 1
-		if not bool(newBoards):
-			found = False
+        sames = set()
+        for board in defs2.InChunks(newBoards, multiprocessing.cpu_count()):
+            same = []
+            # for aBoard in defs2.InChunks(boards,multiprocessing.cpu_count()):
+            # same.append(board & aBoard)
+            sames.update(*pool.starmap(defs2.and_them, zip(itertools.repeat(board),
+                                                           defs2.InChunks(boards, multiprocessing.cpu_count())),
+                                       int(len(newBoards) / multiprocessing.cpu_count()) + 1))
+        # same = set().union(*same)
+        # sames.update(same)
+        newBoards -= sames
 
-		print("Number of combos tested: " + str(number))
-		print("Number of combos unique: " + str(len(boards)))
-		print("Depth: " + str(depth))
-		tmpTime = datetime.today()-timeLast
-		print("Took: "+str(tmpTime))
-		print("Boards per seccond: "+str(newCount/(tmpTime).total_seconds()))
-		timeLast = datetime.today()
-		tmpTime = datetime.today()-timeStart
-		print("Total: "+str(tmpTime))
-		print("Overall boards per seccond: "+str(number/(tmpTime).total_seconds()))
-		print()
+        newBoards = newBoards - boards
+        boards.update(newBoards)
+        number += newCount
+        depth += 1
+        if not bool(newBoards):
+            found = False
 
-	pool.close()
+        print("Number of combos tested: " + str(number))
+        print("Number of combos unique: " + str(len(boards)))
+        print("Depth: " + str(depth))
+        tmpTime = datetime.today() - timeLast
+        print("Took: " + str(tmpTime))
+        print("Boards per seccond: " + str(newCount / (tmpTime).total_seconds()))
+        timeLast = datetime.today()
+        tmpTime = datetime.today() - timeStart
+        print("Total: " + str(tmpTime))
+        print("Overall boards per seccond: " + str(number / (tmpTime).total_seconds()))
+        print()
 
-	print("Number of combos tested:"+str(number))
-	print("Number of unique combos:"+str(len(boards)))
-	print("Depth:"+str(depth-1))
-	tmpTime = datetime.today()-timeStart
-	print("Total Time:"+str(str(tmpTime)))
-	print("Overall boards per seccond: "+str(number/(tmpTime).total_seconds()))
-	print("Total size of boards: "+str(boards.__sizeof__())+" bytes")
-	if checkMem:
-		snapshot = tracemalloc.take_snapshot()
-		defs2.display_top(snapshot)
+    pool.close()
 
-	# file_object  = open("tmp", "w")
-	# [file_object.write(str(i)) for i in oldBoards]
+    print("Number of combos tested:" + str(number))
+    print("Number of unique combos:" + str(len(boards)))
+    print("Depth:" + str(depth - 1))
+    tmpTime = datetime.today() - timeStart
+    print("Total Time:" + str(str(tmpTime)))
+    print("Overall boards per seccond: " + str(number / (tmpTime).total_seconds()))
+    print("Total size of boards: " + str(boards.__sizeof__()) + " bytes")
+    if checkMem:
+        snapshot = tracemalloc.take_snapshot()
+        defs2.display_top(snapshot)
+
+# file_object  = open("tmp", "w")
+# [file_object.write(str(i)) for i in oldBoards]
--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 
-#define N 2
-#define M 2
+#define N 3
+#define M 3
 
-uint_fast8_t** getInitBoard() {
-    auto * values = static_cast<uint_fast8_t *>(calloc(M * N, sizeof(uint_fast8_t)));
-    auto ** rows = static_cast<uint_fast8_t **>(malloc(M * sizeof(uint_fast8_t*)));
-    for (uint_fast8_t i=0; i<M; ++i) {
-        rows[i] = values + i*N;
+inline uint_fast64_t factorial(int n) {
+    uint_fast64_t c, fact = 1;
+    for (c = 1; c <= n; c++) {
+        fact = fact * c;
     }
+    return fact;
+}
+
+void getInitBoard(uint_fast8_t board[N][M]) {
     for (uint_fast8_t i=0; i<M; i++) {
         for(uint_fast8_t j=0; j<N; j++) {
-            rows[i][j] = static_cast<uint_fast8_t>(j + i * N + 1);
+            board[i][j] = static_cast<uint_fast8_t>(j + i * N + 1);
         }
     }
-    return rows;
 }
-uint_fast64_t hashBoard(uint_fast8_t** board) {
+uint_fast64_t hashBoard(uint_fast8_t board[N][M]) {
     uint_fast64_t boardHash = 0;
     for(uint_fast8_t i = 0; i < M; i++) {
         for(uint_fast8_t j = 0; j < N; j++) {
@@ -29,51 +31,161 @@ uint_fast64_t hashBoard(uint_fast8_t** board) {
     }
     return boardHash;
 }
-uint_fast8_t** unHashBoard(uint_fast64_t boardHash) {
+void unHashBoard(uint_fast64_t boardHash, uint_fast8_t board[N][M]) {
     uint_fast64_t currentHash = boardHash;
-    auto * values = static_cast<uint_fast8_t *>(calloc(M * N, sizeof(uint_fast8_t)));
-    auto ** rows = static_cast<uint_fast8_t **>(malloc(M * sizeof(uint_fast8_t*)));
-    for (uint_fast8_t i=0; i<M; ++i) {
-        rows[i] = values + i*N;
-    }
     for (uint_fast8_t i=0; i<M; i++) {
         for(uint_fast8_t j=0; j<N; j++) {
             auto current = static_cast<uint_fast8_t>(currentHash % (M * N));
             if (current) {
-                rows[i][j] = current;
+                board[i][j] = current;
+                currentHash /= N*M;
             } else {
-                rows[i][j] = N*M;
+                board[i][j] = N * M;
+                currentHash /= N*M;
+                currentHash--;
             }
-            currentHash /= N*M;
         }
     }
-    return rows;
 }
-/*inline uint_fast64_t factorial(int n) {
-    uint_fast64_t c, fact = 1;
-    uin
-}*/
 
 int main() {
-    uint_fast8_t** boardTest = getInitBoard();
-    for(uint_fast8_t i = 0; i < M; i++) {
-        for(uint_fast8_t j = 0; j < N; j++) {
-            printf("%d ", boardTest[i][j]);
+    clock_t start = clock();
+
+#if ((N*M)%2)
+    uint_fast64_t numBoards = factorial(N * M);
+#else
+    uint_fast64_t numBoards = factorial(N * M)/2;
+#endif
+
+    auto * boards = static_cast<uint_fast64_t *>(malloc(numBoards * sizeof(uint_fast64_t)));
+    uint_fast64_t lastIndex = 1;
+    auto * currentBoards = static_cast<uint_fast64_t *>(malloc(1 * sizeof(uint_fast64_t)));
+    uint_fast64_t numberCurrentBoards = 1;
+
+    uint_fast8_t initBoard[N][M] = {{0}};
+    getInitBoard(initBoard);
+    boards[0] = hashBoard(initBoard);
+    currentBoards[0] = boards[0];
+
+    uint_fast8_t iteration = 0;
+    bool found = true;
+
+    uint_fast8_t board[N][M] = {{0}};
+    uint_fast8_t tmpBoard[N][M] = {{0}};
+
+    while (found) {
+        iteration++;
+        found = false;
+        auto * nextBoards = static_cast<uint_fast64_t *>(malloc(numberCurrentBoards*(N*2+M*2) * sizeof(uint_fast64_t)));
+        uint_fast64_t nextBoardsIndex = 0;
+        for (uint_fast64_t i = 0; i<numberCurrentBoards; i++) {
+            unHashBoard(currentBoards[i], board);
+
+            //up
+            for (uint_fast8_t j = 0; j<N; j++) {
+                memcpy(tmpBoard, board, sizeof(tmpBoard));
+                for (uint_fast8_t k = 1; k<M; k++) {
+                    tmpBoard[j][k] = board[j][k-1];
+                }
+                tmpBoard[j][0] = board[j][M-1];
+
+                bool inBoards = false;
+                uint_fast64_t tmpBoardHash = hashBoard(tmpBoard);
+                for (uint_fast64_t k = 0; k<lastIndex; k++) {
+                    if (boards[k]==tmpBoardHash) {
+                        inBoards = true;
+                    }
+                }
+                if (!inBoards) {
+                    found = true;
+                    boards[lastIndex++] = tmpBoardHash;
+                    nextBoards[nextBoardsIndex++] = tmpBoardHash;
+                }
+            }
+            //down
+            for (uint_fast8_t j = 0; j<N; j++) {
+                memcpy(tmpBoard, board, sizeof(tmpBoard));
+                for (uint_fast8_t k = 0; k<M-1; k++) {
+                    tmpBoard[j][k] = board[j][k+1];
+                }
+                tmpBoard[j][M-1] = board[j][0];
+
+                bool inBoards = false;
+                uint_fast64_t tmpBoardHash = hashBoard(tmpBoard);
+                for (uint_fast64_t k = 0; k<lastIndex; k++) {
+                    if (boards[k]==tmpBoardHash) {
+                        inBoards = true;
+                    }
+                }
+                if (!inBoards) {
+                    found = true;
+                    boards[lastIndex++] = tmpBoardHash;
+                    nextBoards[nextBoardsIndex++] = tmpBoardHash;
+                }
+            }
+            //left
+            for (uint_fast8_t j = 0; j<M; j++) {
+                memcpy(tmpBoard, board, sizeof(tmpBoard));
+                for (uint_fast8_t k = 0; k<N-1; k++) {
+                    tmpBoard[k][j] = board[k+1][j];
+                }
+                tmpBoard[N-1][j] = board[0][j];
+
+                bool inBoards = false;
+                uint_fast64_t tmpBoardHash = hashBoard(tmpBoard);
+                for (uint_fast64_t k = 0; k<lastIndex; k++) {
+                    if (boards[k]==tmpBoardHash) {
+                        inBoards = true;
+                    }
+                }
+                if (!inBoards) {
+                    found = true;
+                    boards[lastIndex++] = tmpBoardHash;
+                    nextBoards[nextBoardsIndex++] = tmpBoardHash;
+                }
+            }
+            //right
+            for (uint_fast8_t j = 0; j<M; j++) {
+                memcpy(tmpBoard, board, sizeof(tmpBoard));
+                for (uint_fast8_t k = 1; k<N; k++) {
+                    tmpBoard[k][j] = board[k-1][j];
+                }
+                tmpBoard[0][j] = board[N-1][j];
+
+                bool inBoards = false;
+                uint_fast64_t tmpBoardHash = hashBoard(tmpBoard);
+                for (uint_fast64_t k = 0; k<lastIndex; k++) {
+                    if (boards[k]==tmpBoardHash) {
+                        inBoards = true;
+                    }
+                }
+                if (!inBoards) {
+                    found = true;
+                    boards[lastIndex++] = tmpBoardHash;
+                    nextBoards[nextBoardsIndex++] = tmpBoardHash;
+                }
+            }
         }
-        printf("\n");
-    }
-    printf("%llu\n", hashBoard(boardTest));
-    uint_fast8_t** boardTest2 = unHashBoard(hashBoard(boardTest));
-    for(uint_fast8_t i = 0; i < M; i++) {
-        for(uint_fast8_t j = 0; j < N; j++) {
-            printf("%d ", boardTest2[i][j]);
+        free(currentBoards);
+        currentBoards = nextBoards;
+        numberCurrentBoards = nextBoardsIndex;
+        for (uint_fast64_t i = 0; i<numberCurrentBoards; i++) {
+            printf("(");
+            unHashBoard(currentBoards[i], board);
+            for (auto &k : board) {
+                printf("(");
+                for (uint_fast8_t j : k) {
+                    printf("%d, ", j);
+                }
+                printf(")");
+            }
+            printf(")\n");
         }
-        printf("\n");
+        printf("Iteration: %i\n\n",iteration);
     }
 
-    uint_fast64_t numBoards = 0;
-
-    auto * boards = static_cast<uint_fast64_t *>(malloc(M*N * sizeof(uint_fast64_t)));
-
+    clock_t stop = clock();
+    double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
+    printf("\nTime elapsed: %.5f\n", elapsed);
     return 0;
 }
