4024991fda088f25ccbda9b5cc57c9a307660947
Bryce Wilson <bryce@thenetworknerds.ca>
2019-02-11 22:19:39 -0800

    Set types explicitly to the smallest they need to be but using _fast so that it will become larger if that is faster as speed is more important than size.
    Signed-off-by: Bryce Wilson <bryce@thenetworknerds.ca>

--- main.cpp
+++ main.cpp
@@ -3,25 +3,25 @@
 #define N 2
 #define M 2
 
-int** getInitBoard() {
-    int* values = static_cast<int *>(calloc(M * N, sizeof(int)));
-    int** rows = static_cast<int **>(malloc(M * sizeof(int*)));
-    for (int i=0; i<M; ++i) {
+uint_fast8_t** getInitBoard() {
+    auto * values = static_cast<uint_fast8_t *>(calloc(M * N, sizeof(uint_fast8_t)));
+    auto ** rows = static_cast<uint_fast8_t **>(malloc(M * sizeof(uint_fast8_t*)));
+    for (uint_fast8_t i=0; i<M; ++i) {
         rows[i] = values + i*N;
     }
-    for (int i=0; i<M; i++) {
-        for(int j=0; j<N; j++) {
-            rows[i][j] = j + i*N + 1;
+    for (uint_fast8_t i=0; i<M; i++) {
+        for(uint_fast8_t j=0; j<N; j++) {
+            rows[i][j] = static_cast<uint_fast8_t>(j + i * N + 1);
         }
     }
     return rows;
 }
-long hashBoard(int** board) {
-    long boardHash = 0;
-    for(int i = 0; i < M; i++) {
-        for(int j = 0; j < N; j++) {
-            long result = 1;
-            for (int k = 1; k <= (i*N+j); k++) {
+uint_fast64_t hashBoard(uint_fast8_t** board) {
+    uint_fast64_t boardHash = 0;
+    for(uint_fast8_t i = 0; i < M; i++) {
+        for(uint_fast8_t j = 0; j < N; j++) {
+            uint_fast64_t result = 1;
+            for (uint_fast8_t k = 1; k <= (i*N+j); k++) {
                 result *= N*M;
             }
             boardHash += board[i][j]*result;
@@ -29,16 +29,16 @@ long hashBoard(int** board) {
     }
     return boardHash;
 }
-int** unHashBoard(long boardHash) {
-    long currentHash = boardHash;
-    int* values = static_cast<int *>(calloc(M * N, sizeof(int)));
-    int** rows = static_cast<int **>(malloc(M * sizeof(int*)));
-    for (int i=0; i<M; ++i) {
+uint_fast8_t** unHashBoard(uint_fast64_t boardHash) {
+    uint_fast64_t currentHash = boardHash;
+    auto * values = static_cast<uint_fast8_t *>(calloc(M * N, sizeof(uint_fast8_t)));
+    auto ** rows = static_cast<uint_fast8_t **>(malloc(M * sizeof(uint_fast8_t*)));
+    for (uint_fast8_t i=0; i<M; ++i) {
         rows[i] = values + i*N;
     }
-    for (int i=0; i<M; i++) {
-        for(int j=0; j<N; j++) {
-            int current = static_cast<int>(currentHash % (M * N));
+    for (uint_fast8_t i=0; i<M; i++) {
+        for(uint_fast8_t j=0; j<N; j++) {
+            auto current = static_cast<uint_fast8_t>(currentHash % (M * N));
             if (current) {
                 rows[i][j] = current;
             } else {
@@ -49,25 +49,31 @@ int** unHashBoard(long boardHash) {
     }
     return rows;
 }
+/*inline uint_fast64_t factorial(int n) {
+    uint_fast64_t c, fact = 1;
+    uin
+}*/
 
 int main() {
-    int** boardTest = getInitBoard();
-    for(int i = 0; i < M; i++) {
-        for(int j = 0; j < N; j++) {
+    uint_fast8_t** boardTest = getInitBoard();
+    for(uint_fast8_t i = 0; i < M; i++) {
+        for(uint_fast8_t j = 0; j < N; j++) {
             printf("%d ", boardTest[i][j]);
         }
         printf("\n");
     }
-    printf("%li\n", hashBoard(boardTest));
-    int** boardTest2 = unHashBoard(hashBoard(boardTest));
-    for(int i = 0; i < M; i++) {
-        for(int j = 0; j < N; j++) {
+    printf("%llu\n", hashBoard(boardTest));
+    uint_fast8_t** boardTest2 = unHashBoard(hashBoard(boardTest));
+    for(uint_fast8_t i = 0; i < M; i++) {
+        for(uint_fast8_t j = 0; j < N; j++) {
             printf("%d ", boardTest2[i][j]);
         }
         printf("\n");
     }
 
+    uint_fast64_t numBoards = 0;
 
+    auto * boards = static_cast<uint_fast64_t *>(malloc(M*N * sizeof(uint_fast64_t)));
 
     return 0;
 }
